03335dc7e950784db1d94ed608551691b0f26d66
coco3244 <colin.nemeghaire@hotmail.com>
2022-02-05 13:14:19 +0100

    tableau de résultat

--- build/calc.js
+++ build/calc.js
@@ -9,32 +9,72 @@ var quantainumTR = document.querySelector('.quantainum');
 var lineTabl = document.querySelectorAll('.lineTab');
 var jobTR = document.querySelector('.job');
 var thJob;
+var thJobRes;
+/**
+ * Ajout de la ligne du minerai selectionné
+ */
+
 addButton.addEventListener('click', function (event) {
-  document.querySelector(".".concat(listeMinerai.value)).classList.remove('hide');
+  var classes = document.querySelectorAll(".".concat(listeMinerai.value));
+  classes.forEach(function (value) {
+    value.classList.remove('hide');
+  });
 });
+/**
+ * Suppression de la ligne du minerai selectionné
+ */
+
 remButton.addEventListener('click', function (event) {
-  document.querySelector(".".concat(listeMinerai.value)).classList.add('hide');
+  var classes = document.querySelectorAll(".".concat(listeMinerai.value));
+  classes.forEach(function (value) {
+    value.classList.add('hide');
+  });
 });
+/**
+ * Ajout d'une colonne de job
+ */
+
 boutonPlus.addEventListener('click', function (event) {
   thJob = document.querySelectorAll('.thJob');
   lineTabl.forEach(function (value) {
     if (value.classList.contains('job')) {
       var newTh = document.createElement('th');
-      newTh.classList.add('thJob');
+
+      if (!value.classList.contains('resLine')) {
+        newTh.classList.add('thJob');
+      } else {
+        newTh.classList.add('thJobRes');
+      }
+
       newTh.classList.add("job".concat(thJob.length + 1));
       newTh.innerHTML = "Job ".concat(thJob.length + 1);
-      var button = document.createElement('button');
-      button.classList.add('suppbtn');
-      button.innerHTML = 'X';
-      newTh.appendChild(button);
+
+      if (!value.classList.contains('resLine')) {
+        var button = document.createElement('button');
+        button.classList.add('suppbtn');
+        button.innerHTML = 'X';
+        newTh.appendChild(button);
+      }
+
       value.appendChild(newTh);
     } else {
       var newTd = document.createElement('td');
       newTd.classList.add("job".concat(thJob.length + 1));
-      var saisie = document.createElement('input');
-      saisie.setAttribute('type', 'number');
-      saisie.setAttribute('min', '0');
-      newTd.appendChild(saisie);
+
+      if (value.classList.contains('resLine')) {
+        var saisie = document.createElement('H2');
+        saisie.innerHTML = '0';
+        newTd.appendChild(saisie);
+      } else {
+        var _saisie = document.createElement('input');
+
+        _saisie.setAttribute('type', 'number');
+
+        _saisie.setAttribute('min', '0');
+
+        newTd.appendChild(_saisie);
+      }
+
       value.appendChild(newTd);
     }
   });
@@ -48,14 +88,20 @@ jobTR.addEventListener('click', function (event) {
     toutLeBordel.forEach(function (value) {
       value.remove();
     });
-    console.log(lineTabl);
     var count = 1;
     lineTabl.forEach(function (value) {
       var count2 = 1;
+      var ths = value.querySelectorAll('th'); // console.warn(ths);     
 
-      if (count === 1) {
-        var ths = value.querySelectorAll('th');
-        ths.forEach(function (vals) {
+      ths.forEach(function (vals) {
+        console.log(vals);
+
+        if (vals.classList.contains('thJobRes')) {
+          vals.className = "";
+          vals.classList.add("thJobRes");
+          vals.classList.add("job".concat(count2));
+          vals.innerHTML = "job ".concat(count2);
+        } else {
           vals.className = "";
           vals.classList.add("thJob");
           vals.classList.add("job".concat(count2));
@@ -65,24 +111,21 @@ jobTR.addEventListener('click', function (event) {
           button.classList.add("SuppButton".concat(count2));
           button.innerHTML = 'X';
           vals.appendChild(button);
+        }
+
+        count2++;
+      });
+      var tds = value.querySelectorAll('td');
+      var count3 = 1;
+      tds.forEach(function (vals) {
+        if (count3 != 1) {
+          vals.className = "";
+          vals.classList.add("job".concat(count2));
           count2++;
-        });
-      } else {
-        var _ths = value.querySelectorAll('td');
+        }
 
-        var count3 = 1;
-
-        _ths.forEach(function (vals) {
-          if (count3 != 1) {
-            vals.className = "";
-            vals.classList.add("job".concat(count2));
-            count2++;
-          }
-
-          count3++;
-        });
-      }
-
+        count3++;
+      });
       count++;
     });
   }
--- src/calc.js
+++ src/calc.js
@@ -8,34 +8,65 @@ const listeMinerai= document.querySelector('.listeMinerai');
 const quantainumTR=document.querySelector('.quantainum');
 const lineTabl = document.querySelectorAll('.lineTab');
 const jobTR = document.querySelector('.job');
-let thJob
+let thJob;
+let thJobRes;
+
+/**
+ * Ajout de la ligne du minerai selectionné
+ */
 addButton.addEventListener('click',(event)=>{
-    document.querySelector(`.${listeMinerai.value}`).classList.remove('hide');
+    let classes = document.querySelectorAll(`.${listeMinerai.value}`);
+    classes.forEach(value=>{
+        value.classList.remove('hide')
+    })
+
 })
+/**
+ * Suppression de la ligne du minerai selectionné
+ */
 remButton.addEventListener('click',(event)=>{
-    document.querySelector(`.${listeMinerai.value}`).classList.add('hide');
+    let classes = document.querySelectorAll(`.${listeMinerai.value}`);
+    classes.forEach(value=>{
+        value.classList.add('hide');
+    })
 })
 
+/**
+ * Ajout d'une colonne de job
+ */
 boutonPlus.addEventListener('click',(event)=>{  
     thJob = document.querySelectorAll('.thJob');
     lineTabl.forEach(value=>{
         if(value.classList.contains('job')){
             const newTh = document.createElement('th');
-            newTh.classList.add('thJob');
+            if(!value.classList.contains('resLine')){
+                newTh.classList.add('thJob');
+            }else{
+                newTh.classList.add('thJobRes');
+            }         
             newTh.classList.add(`job${thJob.length+1}`);
             newTh.innerHTML=`Job ${thJob.length+1}`;
-            const button = document.createElement('button');
-            button.classList.add('suppbtn');
-            button.innerHTML='X';
-            newTh.appendChild(button);
+            if(!value.classList.contains('resLine')){
+                const button = document.createElement('button');
+                button.classList.add('suppbtn');
+                button.innerHTML='X';
+                newTh.appendChild(button);
+            }
             value.appendChild(newTh);
         }else{
             const newTd = document.createElement('td');
             newTd.classList.add(`job${thJob.length+1}`);
-            const saisie = document.createElement('input');
-            saisie.setAttribute('type','number');
-            saisie.setAttribute('min','0');
-            newTd.appendChild(saisie);
+            if(value.classList.contains('resLine')){
+                const saisie = document.createElement('H2');
+                saisie.innerHTML='0';
+                newTd.appendChild(saisie);
+            }else{
+                const saisie = document.createElement('input');
+                saisie.setAttribute('type','number');
+                saisie.setAttribute('min','0');
+                newTd.appendChild(saisie);
+            }
+            
             value.appendChild(newTd);
         }
         
@@ -50,38 +81,42 @@ jobTR.addEventListener('click',(event)=>{
         const classeDuParent = event.target.parentNode.classList[1];
         
         const toutLeBordel = document.querySelectorAll(`.${classeDuParent}`);
-        
         toutLeBordel.forEach(value =>{
             value.remove();
         })
         
-        
-        console.log(lineTabl);
         let count = 1;
         lineTabl.forEach(value => {           
             let count2=1;
-
-            if(count===1){
-                const ths = value.querySelectorAll('th');
-
+                const ths = value.querySelectorAll('th'); 
+               // console.warn(ths);     
                 ths.forEach(vals=>{
-                    vals.className="";
-                    vals.classList.add(`thJob`);
-                    vals.classList.add(`job${count2}`);
-                    vals.innerHTML=`job ${count2}`;
-                    const button = document.createElement('button');
-                    button.classList.add('suppbtn');
-                    button.classList.add(`SuppButton${count2}`);
-                    button.innerHTML='X';
-                    vals.appendChild(button);
+                    console.log(vals);
+                    if(vals.classList.contains('thJobRes')){
+                        vals.className="";
+                        vals.classList.add(`thJobRes`);
+                        vals.classList.add(`job${count2}`);
+                        vals.innerHTML=`job ${count2}`;
+                    }else{
+                        vals.className="";
+                        vals.classList.add(`thJob`);
+                        vals.classList.add(`job${count2}`);
+                        vals.innerHTML=`job ${count2}`;
+                        const button = document.createElement('button');
+                        button.classList.add('suppbtn');
+                        button.classList.add(`SuppButton${count2}`);
+                        button.innerHTML='X';
+                        vals.appendChild(button);
+                    }
+                       
+                    
+                    
                     count2++;
                 })
-         
-            }else{
-                
-                const ths = value.querySelectorAll('td');
+                        
+                const tds = value.querySelectorAll('td');
                 let count3=1;
-                ths.forEach(vals=>{
+                tds.forEach(vals=>{
                     if(count3!=1){
                         vals.className="";
                         vals.classList.add(`job${count2}`);              
@@ -91,7 +126,7 @@ jobTR.addEventListener('click',(event)=>{
                 
                 })
 
-            }
+            
             count++;
         });
         
