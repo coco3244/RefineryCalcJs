9919c41e0a6b9a289fce274787131110e7ac4468
coco3244 <colin.nemeghaire@hotmail.com>
2022-02-09 18:31:13 +0100

    mtn ca marche vraiment le scoll

--- build/scriptCalc.js
+++ build/scriptCalc.js
@@ -1,2 +1,8 @@
 "use strict";
+
+var scrollContainer = document.querySelector('.jobsContainer');
+scrollContainer.addEventListener('wheel', function (event) {
+  event.preventDefault();
+  scrollContainer.scrollLeft += event.deltaY;
+});
 //# sourceMappingURL=scriptCalc.js.map
