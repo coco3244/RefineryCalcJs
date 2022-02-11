5108def0bff35e521e286189c5db2da051a118a3
coco3244 <colin.nemeghaire@hotmail.com>
2022-02-11 14:23:33 +0100

    bug fix

--- build/scriptCalc.js
+++ build/scriptCalc.js
@@ -3,28 +3,21 @@
 var scrollContainer = document.querySelector('.jobsContainer');
 var addJobButton = document.querySelector('.addJobCont');
 var selectFiltre = document.querySelector("#selectFiltre");
+var jobsContainer = document.querySelector(".jobsContainer");
 scrollContainer.addEventListener('wheel', function (event) {
   event.preventDefault();
   scrollContainer.scrollLeft += event.deltaY;
 });
-addJobButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  var jobsContainer = document.querySelector(".jobsContainer");
-  var numJob = document.querySelectorAll(".job").length + 1;
-  var jobHtml =
-  /*html*/
-  "\n    <div class=\"job job".concat(numJob, "\" id=\"jobId_TOBECHANGED\">\n        <label class=\"titleJob dontmod\">").concat(numJob, "</label>\n\n        <div class=\"mineraisContainer\">\n                    <select class=\"selectMinerai\"> \n                    <option>quantainum</option>\n                    <option>bexalite</option>\n                    <option>taranite</option>\n                    <option>borase</option>\n                    <option>laranite</option>\n                    <option>agricium</option>\n                    <option>hephaestanite</option>\n                    <option>titanium</option>                               \n                    </select>\n                <button class=\"btnAddMineral\">Ajouter</button>\n                <button class=\"btnSuppMineral\">Supprimer</button>\n            <div class=\"mineraisJob\">\n                \n                \n                <div class=\"listeMinerais\">\n                    \n                </div>\n\n                <span class=\"separate\"></span>\n                <div class=\"listeQuantites\">\n                    \n                </div>\n            </div>\n            <label class=\"titreCat dontmod\">Total de ce Raffinage : </label>\n            <div class=\"tabCat\">\n                0\n            </div>\n        </div>\n        \n        <div class=\"emplacementContainer\">\n            <label class=\"titreCat dontmod\">Emplacement : </label>\n            <div class=\"tabCat\">\n                <select class=\"Raffinery\"> \n                    <option>CRU-L1</option>\n                    <option>ARC-L1</option>\n                    <option>ARC-L2</option>\n                    <option>HUR-L1</option>\n                    <option>HUR-L2</option>\n                    <option>MIC-L2</option>                             \n                </select>\n            </div>\n        </div>\n\n        <div class=\"tempsContainer\">\n            <label class=\"titreCat dontmod\">Temps Restant : </label>\n            <div class=\"tabCat\">\n                <input class=\"temprestant\" type=\"text\">\n            </div>\n        </div>\n\n        <div class=\"btnsContainer\">\n            <button class=\"btnTransport\">Transporter</button>\n            <button class=\"btnModif hide\">Modifier</button>\n            <button class=\"btnConfirm\">Confirmer</button>\n        </div>\n    </div>");
-  jobsContainer.innerHTML = jobHtml + jobsContainer.innerHTML;
-  var newJob = document.querySelector(".job".concat(numJob));
-  var selectMinerai = newJob.querySelector('.selectMinerai');
-  var listeMineraisDiv = newJob.querySelector('.listeMinerais');
-  var listeQuantitesDiv = newJob.querySelector('.listeQuantites');
-  /**
-  * listener pour ajouter un minerai
-  * ca cree un label et un input et ca les insère dans leur div correspondante
-  */
+jobContainer.addEventListener("click", function (event) {
+  var selectMinerai = event.target.parentNode.parentNode.querySelector('.selectMinerai');
+  var listeMineraisDiv = event.target.parentNode.parentNode.querySelector('.listeMinerais');
+  var listeQuantitesDiv = event.target.parentNode.parentNode.querySelector('.listeQuantites');
 
-  newJob.querySelector('.btnAddMineral').addEventListener('click', function (event) {
+  if (event.target.classList.contains("btnAddMineral")) {
+    /**
+    * listener pour ajouter un minerai
+    * ca cree un label et un input et ca les insère dans leur div correspondante
+    */
     if (!listeMineraisDiv.querySelector(".".concat(selectMinerai.value))) {
       var input = document.createElement('input');
       input.setAttribute('type', 'number');
@@ -36,40 +29,42 @@ addJobButton.addEventListener("click", function (event) {
       listeQuantitesDiv.appendChild(input);
       listeMineraisDiv.appendChild(label);
     }
-  });
-  /**
-  * Supprime les minerai des 2 div correspondante
-  */
+  }
 
-  newJob.querySelector('.btnSuppMineral').addEventListener('click', function (event) {
+  if (event.target.classList.contains("btnSuppMineral")) {
+    /**
+    * Supprime les minerai des 2 div correspondante
+    */
     listeQuantitesDiv.querySelector(".".concat(selectMinerai.value)).remove();
     listeMineraisDiv.querySelector(".".concat(selectMinerai.value)).remove();
-  });
-  /**
-  * listener du bouton confirmation
-  * ça confirme la modification
-  * ca re-affiche le menu déroulant des minerai et les bouton ajouter/supprimer
-  * ca cache le bouton modifier et re affiche le bouton confirmer
-  */
+  }
 
-  newJob.querySelector('.btnModif').addEventListener('click', function (event) {
-    var labels = newJob.querySelectorAll('label');
+  if (event.target.classList.contains("btnModif")) {
+    /**
+    * listener du bouton confirmation
+    * ça confirme la modification
+    * ca re-affiche le menu déroulant des minerai et les bouton ajouter/supprimer
+    * ca cache le bouton modifier et re affiche le bouton confirmer
+    */
+    event.target.parentNode.querySelector('.btnConfirm').classList.remove('hide');
+    event.target.parentNode.querySelector('.btnModif').classList.add('hide');
+    var labels = event.target.parentNode.parentNode.querySelectorAll('label');
     selectMinerai.classList.remove('hide');
-    newJob.querySelector('.btnAddMineral').classList.remove('hide');
-    newJob.querySelector('.btnSuppMineral').classList.remove('hide');
+    event.target.parentNode.parentNode.querySelector('.btnAddMineral').classList.remove('hide');
+    event.target.parentNode.parentNode.querySelector('.btnSuppMineral').classList.remove('hide');
     labels.forEach(function (label) {
       if (!label.classList.contains('dontmod')) {
-        var input = document.createElement('input');
+        var _input = document.createElement('input');
 
         if (label.parentNode.classList.contains('listeQuantites')) {
-          input.setAttribute('type', 'number');
+          _input.setAttribute('type', 'number');
         } else {
-          input.setAttribute('type', 'text');
+          _input.setAttribute('type', 'text');
         }
 
         if (label.classList.contains('Raffinery')) {
           var select = document.createElement('select');
-          select.innerHTML = "\n                <option>CRU-L1</option>\n                <option>ARC-L1</option>\n                <option>ARC-L2</option>\n                <option>HUR-L1</option>\n                <option>HUR-L2</option>\n                <option>MIC-L2</option>                             \n                ";
+          select.innerHTML = "\n                    <option>CRU-L1</option>\n                    <option>ARC-L1</option>\n                    <option>ARC-L2</option>\n                    <option>HUR-L1</option>\n                    <option>HUR-L2</option>\n                    <option>MIC-L2</option>                             \n                    ";
           select.classList.add('Raffinery');
           var options = select.querySelectorAll('option');
           var raffinerie = label.innerHTML;
@@ -81,29 +76,27 @@ addJobButton.addEventListener("click", function (event) {
           label.parentNode.appendChild(select);
           label.remove();
         } else {
-          input.classList.add("".concat(label.classList.toString()));
-          input.setAttribute('value', "".concat(delUnit(label.innerHTML, 5)));
-          label.parentNode.appendChild(input);
+          _input.classList.add("".concat(label.classList.toString()));
+
+          _input.setAttribute('value', "".concat(delUnit(label.innerHTML, 5)));
+
+          label.parentNode.appendChild(_input);
           label.remove();
         }
       }
     });
-    var target = event.target.parentNode;
-    target.querySelector('.btnConfirm').classList.remove('hide');
-    target.querySelector('.btnModif').classList.add('hide');
-  });
-  /**
+  }
+
+  if (event.target.classList.contains("btnConfirm")) {
+    /**
     * listener pour la confirmation de modification
     * ca cache le menu déroulant avec les minerai ainsi que les boutons ajouter et supprimer
     * ca transforme les input en label
     * ca cache le bouton confirmer et re affiche le bouton modifier
     */
-
-  newJob.querySelector('.btnConfirm').addEventListener('click', function (event) {
-    var target = event.target.parentNode;
-    target.querySelector('.btnConfirm').classList.add('hide');
-    target.querySelector('.btnModif').classList.remove('hide');
-    var inputs = newJob.querySelectorAll('input');
+    event.target.parentNode.querySelector('.btnConfirm').classList.add('hide');
+    event.target.parentNode.querySelector('.btnModif').classList.remove('hide');
+    var inputs = event.target.parentNode.parentNode.querySelectorAll('input');
     inputs.forEach(function (input) {
       var label = document.createElement('label');
       label.classList.add("".concat(input.classList.toString()));
@@ -117,20 +110,30 @@ addJobButton.addEventListener("click", function (event) {
       input.parentNode.appendChild(label);
       input.remove();
     });
-    var raffinerySelect = newJob.querySelector('.Raffinery');
-    var label = document.createElement('label');
-    console.log(newJob);
-    label.classList.add("".concat(raffinerySelect.classList.toString()));
-    label.innerHTML = raffinerySelect.value;
-    raffinerySelect.parentNode.appendChild(label);
+    var raffinerySelect = event.target.parentNode.parentNode.querySelector('.Raffinery');
+
+    var _label = document.createElement('label');
+
+    _label.classList.add("".concat(raffinerySelect.classList.toString()));
+
+    _label.innerHTML = raffinerySelect.value;
+    raffinerySelect.parentNode.appendChild(_label);
     raffinerySelect.remove();
     selectMinerai.classList.add('hide');
-    newJob.querySelector('.btnAddMineral').classList.add('hide');
-    newJob.querySelector('.btnSuppMineral').classList.add('hide');
-  });
+    event.target.parentNode.parentNode.querySelector('.btnAddMineral').classList.add('hide');
+    event.target.parentNode.parentNode.querySelector('.btnSuppMineral').classList.add('hide');
+  }
+});
+addJobButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  var numJob = document.querySelectorAll(".job").length + 1;
+  var jobHtml =
+  /*html*/
+  "\n    <div class=\"job job".concat(numJob, "\" id=\"jobId_TOBECHANGED\">\n        <label class=\"titleJob dontmod\">").concat(numJob, "</label>\n\n        <div class=\"mineraisContainer\">\n                    <select class=\"selectMinerai\"> \n                    <option>quantainum</option>\n                    <option>bexalite</option>\n                    <option>taranite</option>\n                    <option>borase</option>\n                    <option>laranite</option>\n                    <option>agricium</option>\n                    <option>hephaestanite</option>\n                    <option>titanium</option>                               \n                    </select>\n                <button class=\"btnAddMineral\">Ajouter</button>\n                <button class=\"btnSuppMineral\">Supprimer</button>\n            <div class=\"mineraisJob\">\n                \n                \n                <div class=\"listeMinerais\">\n                    \n                </div>\n\n                <span class=\"separate\"></span>\n                <div class=\"listeQuantites\">\n                    \n                </div>\n            </div>\n            <label class=\"titreCat dontmod\">Total de ce Raffinage : </label>\n            <div class=\"tabCat\">\n                0\n            </div>\n        </div>\n        \n        <div class=\"emplacementContainer\">\n            <label class=\"titreCat dontmod\">Emplacement : </label>\n            <div class=\"tabCat\">\n                <select class=\"Raffinery\"> \n                    <option>CRU-L1</option>\n                    <option>ARC-L1</option>\n                    <option>ARC-L2</option>\n                    <option>HUR-L1</option>\n                    <option>HUR-L2</option>\n                    <option>MIC-L2</option>                             \n                </select>\n            </div>\n        </div>\n\n        <div class=\"tempsContainer\">\n            <label class=\"titreCat dontmod\">Temps Restant : </label>\n            <div class=\"tabCat\">\n                <input class=\"temprestant\" type=\"text\">\n            </div>\n        </div>\n\n        <div class=\"btnsContainer\">\n            <button class=\"btnTransport\">Transporter</button>\n            <button class=\"btnModif hide\">Modifier</button>\n            <button class=\"btnConfirm\">Confirmer</button>\n        </div>\n    </div>");
+  jobsContainer.innerHTML = jobHtml + jobsContainer.innerHTML;
 });
 selectFiltre.addEventListener("input", function (e) {
-  console.log(selectFiltre.value);
+  //console.log(selectFiltre.value);
   var pseudoCont = document.querySelector("#pseudoAct");
   var pseudo = pseudoCont.innerHTML;
   fetchDB(pseudo, selectFiltre.value);
@@ -147,5 +150,26 @@ function delUnit(numb, nbUnit) {
   numb.splice(numb.length - nbUnit, nbUnit);
   numb = numb.join("");
   return Number(numb);
+}
+/**
+ * fonction fournie par Liduen
+ * @param {*} classF 
+ * @param {*} e 
+ * @returns 
+ */
+
+
+function find(classF, e) {
+  var targ = e.target;
+
+  while (!targ.classList.contains(classF)) {
+    targ = targ.parentNode;
+
+    if (targ === document.body) {
+      break;
+    }
+  }
+
+  return targ;
 }
 //# sourceMappingURL=scriptCalc.js.map
--- src/scriptCalc.js
+++ src/scriptCalc.js
@@ -1,16 +1,150 @@
 const scrollContainer = document.querySelector('.jobsContainer');
 const addJobButton = document.querySelector('.addJobCont');
 const selectFiltre = document.querySelector("#selectFiltre");
+const jobsContainer = document.querySelector(".jobsContainer");
+
 
 scrollContainer.addEventListener('wheel',event=>{
     event.preventDefault();
     scrollContainer.scrollLeft += event.deltaY;
 });
 
+jobContainer.addEventListener("click", (event) => {
+    const selectMinerai = event.target.parentNode.parentNode.querySelector('.selectMinerai');
+    const listeMineraisDiv = event.target.parentNode.parentNode.querySelector('.listeMinerais');
+    const listeQuantitesDiv = event.target.parentNode.parentNode.querySelector('.listeQuantites');
+    if(event.target.classList.contains("btnAddMineral")) {
+        /**
+        * listener pour ajouter un minerai
+        * ca cree un label et un input et ca les insère dans leur div correspondante
+        */                          
+        if(!listeMineraisDiv.querySelector(`.${selectMinerai.value}`)){
+            const input = document.createElement('input');
+            input.setAttribute('type','number');
+            input.classList.add(`${selectMinerai.value}`);
+            
+            const label = document.createElement('label');
+            label.classList.add(`${selectMinerai.value}`);
+            label.classList.add(`dontmod`);
+            label.innerHTML=`${selectMinerai.value}`;
+            
+            listeQuantitesDiv.appendChild(input);
+            listeMineraisDiv.appendChild(label);
+        }     
+        
+        
+    }
+    if(event.target.classList.contains("btnSuppMineral")) {
+        /**
+        * Supprime les minerai des 2 div correspondante
+        */                
+        listeQuantitesDiv.querySelector(`.${selectMinerai.value}`).remove();
+        listeMineraisDiv.querySelector(`.${selectMinerai.value}`).remove();   
+        
+    }
+    if(event.target.classList.contains("btnModif")) {
+        /**
+        * listener du bouton confirmation
+        * ça confirme la modification
+        * ca re-affiche le menu déroulant des minerai et les bouton ajouter/supprimer
+        * ca cache le bouton modifier et re affiche le bouton confirmer
+        */
+        event.target.parentNode.querySelector('.btnConfirm').classList.remove('hide');
+        event.target.parentNode.querySelector('.btnModif').classList.add('hide');
+        
+        const labels = event.target.parentNode.parentNode.querySelectorAll('label');
+        selectMinerai.classList.remove('hide');
+        event.target.parentNode.parentNode.querySelector('.btnAddMineral').classList.remove('hide');
+        event.target.parentNode.parentNode.querySelector('.btnSuppMineral').classList.remove('hide');
+        
+        labels.forEach(label=>{
+            
+            if(!label.classList.contains('dontmod')){
+                const input = document.createElement('input');
+                if(label.parentNode.classList.contains('listeQuantites')){
+                    input.setAttribute('type','number');
+                }else{
+                    input.setAttribute('type','text');
+                }
+                
+                if(label.classList.contains('Raffinery')){
+                    const select = document.createElement('select');
+                    select.innerHTML=`
+                    <option>CRU-L1</option>
+                    <option>ARC-L1</option>
+                    <option>ARC-L2</option>
+                    <option>HUR-L1</option>
+                    <option>HUR-L2</option>
+                    <option>MIC-L2</option>                             
+                    `
+                    select.classList.add('Raffinery')
+                    const options = select.querySelectorAll('option');
+                    const raffinerie = label.innerHTML;
+                    options.forEach(value=>{
+                        if(value.innerHTML===raffinerie){
+                            select.value=raffinerie;
+                        }
+                    });
+                    label.parentNode.appendChild(select)
+                    label.remove();
+                }else{
+                    input.classList.add(`${label.classList.toString()}`);
+                    input.setAttribute('value',`${delUnit(label.innerHTML,5)}`)
+                    label.parentNode.appendChild(input);
+                    label.remove();
+                }
+                
+            }
+        })
+        
+       
+        
+    }
+    if(event.target.classList.contains("btnConfirm")) {
+        /**
+        * listener pour la confirmation de modification
+        * ca cache le menu déroulant avec les minerai ainsi que les boutons ajouter et supprimer
+        * ca transforme les input en label
+        * ca cache le bouton confirmer et re affiche le bouton modifier
+        */
+        event.target.parentNode.querySelector('.btnConfirm').classList.add('hide');
+        event.target.parentNode.querySelector('.btnModif').classList.remove('hide');
+        
+        const inputs = event.target.parentNode.parentNode.querySelectorAll('input');
+        inputs.forEach(input=>{
+            const label = document.createElement('label');
+            
+            label.classList.add(`${input.classList.toString()}`)
+            if(!label.classList.contains('temprestant')){
+                label.innerHTML=`${input.value} cSCU`;
+            }else{
+                label.innerHTML=`${input.value}`;
+            }
+            
+            input.parentNode.appendChild(label);
+            input.remove();
+        })
+        
+        const raffinerySelect = event.target.parentNode.parentNode.querySelector('.Raffinery')
+        const label = document.createElement('label');
+        
+        label.classList.add(`${raffinerySelect.classList.toString()}`)
+        label.innerHTML=raffinerySelect.value
+        raffinerySelect.parentNode.appendChild(label)
+        raffinerySelect.remove();
+        selectMinerai.classList.add('hide');
+        
+        event.target.parentNode.parentNode.querySelector('.btnAddMineral').classList.add('hide');
+        event.target.parentNode.parentNode.querySelector('.btnSuppMineral').classList.add('hide');
+        
+    }
+});
+
+
+
+
 addJobButton.addEventListener("click", (event) => {
     event.preventDefault();
-    const jobsContainer = document.querySelector(".jobsContainer");
-    
     const numJob = document.querySelectorAll(".job").length + 1;
     let jobHtml = /*html*/ `
     <div class="job job${numJob}" id="jobId_TOBECHANGED">
@@ -75,144 +209,11 @@ addJobButton.addEventListener("click", (event) => {
         </div>
     </div>`;
   jobsContainer.innerHTML = jobHtml + jobsContainer.innerHTML;
-  const newJob = document.querySelector(`.job${numJob}`);
-  
-  const selectMinerai = newJob.querySelector('.selectMinerai');
-  const listeMineraisDiv = newJob.querySelector('.listeMinerais');
-  const listeQuantitesDiv = newJob.querySelector('.listeQuantites');
-  
-
-/**
-* listener pour ajouter un minerai
-* ca cree un label et un input et ca les insère dans leur div correspondante
-*/
-newJob.querySelector('.btnAddMineral').addEventListener('click',event=>{  
-    if(!listeMineraisDiv.querySelector(`.${selectMinerai.value}`)){
-        const input = document.createElement('input');
-        input.setAttribute('type','number');
-        input.classList.add(`${selectMinerai.value}`);
-        
-        const label = document.createElement('label');
-        label.classList.add(`${selectMinerai.value}`);
-        label.classList.add(`dontmod`);
-        label.innerHTML=`${selectMinerai.value}`;
-        
-        listeQuantitesDiv.appendChild(input);
-        listeMineraisDiv.appendChild(label);
-    }
-    
-    
-}); 
-/**
-* Supprime les minerai des 2 div correspondante
-*/
-newJob.querySelector('.btnSuppMineral').addEventListener('click',event=>{    
-    listeQuantitesDiv.querySelector(`.${selectMinerai.value}`).remove();
-    listeMineraisDiv.querySelector(`.${selectMinerai.value}`).remove();   
-}); 
-
-/**
-* listener du bouton confirmation
-* ça confirme la modification
-* ca re-affiche le menu déroulant des minerai et les bouton ajouter/supprimer
-* ca cache le bouton modifier et re affiche le bouton confirmer
-*/
-newJob.querySelector('.btnModif').addEventListener('click',event=>{  
-    
-    const labels = newJob.querySelectorAll('label');
-    selectMinerai.classList.remove('hide');
-    newJob.querySelector('.btnAddMineral').classList.remove('hide');
-    newJob.querySelector('.btnSuppMineral').classList.remove('hide');
-    
-    labels.forEach(label=>{
-        
-        if(!label.classList.contains('dontmod')){
-            const input = document.createElement('input');
-            if(label.parentNode.classList.contains('listeQuantites')){
-                input.setAttribute('type','number');
-            }else{
-                input.setAttribute('type','text');
-            }
-            
-            if(label.classList.contains('Raffinery')){
-                const select = document.createElement('select');
-                select.innerHTML=`
-                <option>CRU-L1</option>
-                <option>ARC-L1</option>
-                <option>ARC-L2</option>
-                <option>HUR-L1</option>
-                <option>HUR-L2</option>
-                <option>MIC-L2</option>                             
-                `
-                select.classList.add('Raffinery')
-                const options = select.querySelectorAll('option');
-                const raffinerie = label.innerHTML;
-                options.forEach(value=>{
-                    if(value.innerHTML===raffinerie){
-                        select.value=raffinerie;
-                    }
-                });
-                label.parentNode.appendChild(select)
-                label.remove();
-            }else{
-                input.classList.add(`${label.classList.toString()}`);
-                input.setAttribute('value',`${delUnit(label.innerHTML,5)}`)
-                label.parentNode.appendChild(input);
-                label.remove();
-            }
-            
-        }
-    })
-    
-    const target = event.target.parentNode;
-    target.querySelector('.btnConfirm').classList.remove('hide');
-    target.querySelector('.btnModif').classList.add('hide');
-}); 
-
-/**
-  * listener pour la confirmation de modification
-  * ca cache le menu déroulant avec les minerai ainsi que les boutons ajouter et supprimer
-  * ca transforme les input en label
-  * ca cache le bouton confirmer et re affiche le bouton modifier
-  */
- newJob.querySelector('.btnConfirm').addEventListener('click',event=>{  
-    const target = event.target.parentNode;
-    target.querySelector('.btnConfirm').classList.add('hide');
-    target.querySelector('.btnModif').classList.remove('hide');
-    
-    const inputs = newJob.querySelectorAll('input');
-    inputs.forEach(input=>{
-        const label = document.createElement('label');
-        
-        label.classList.add(`${input.classList.toString()}`)
-        if(!label.classList.contains('temprestant')){
-            label.innerHTML=`${input.value} cSCU`;
-        }else{
-            label.innerHTML=`${input.value}`;
-        }
-        
-        input.parentNode.appendChild(label);
-        input.remove();
-    })
-    
-    const raffinerySelect = newJob.querySelector('.Raffinery')
-    const label = document.createElement('label');
-    console.log(newJob);
-    label.classList.add(`${raffinerySelect.classList.toString()}`)
-    label.innerHTML=raffinerySelect.value
-    raffinerySelect.parentNode.appendChild(label)
-    raffinerySelect.remove();
-
-    selectMinerai.classList.add('hide');
-    newJob.querySelector('.btnAddMineral').classList.add('hide');
-    newJob.querySelector('.btnSuppMineral').classList.add('hide');
-}); 
-
 });
 
 
 selectFiltre.addEventListener("input", e => {
-    console.log(selectFiltre.value);
+    //console.log(selectFiltre.value);
     const pseudoCont = document.querySelector("#pseudoAct");
     const pseudo = pseudoCont.innerHTML;
     fetchDB(pseudo, selectFiltre.value);
@@ -231,4 +232,19 @@ function delUnit(numb, nbUnit) {
     numb.splice((numb.length - nbUnit), nbUnit);
     numb = numb.join("");
     return Number(numb);
+}
+/**
+ * fonction fournie par Liduen
+ * @param {*} classF 
+ * @param {*} e 
+ * @returns 
+ */
+function find(classF, e) {
+    let targ = e.target;
+    while(!targ.classList.contains(classF)) {
+        targ = targ.parentNode;
+
+        if(targ === document.body) {break;}
+    } 
+    return targ;
 }
